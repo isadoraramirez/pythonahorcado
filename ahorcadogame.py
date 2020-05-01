5c44294c8af170e63609a2a8c46c783da25583d9
isadoraramirez <mydemoni@hotmail.com>
2020-05-01 15:43:50 -0500

    corigiendo errores

--- ahorcadogame.py
+++ ahorcadogame.py
@@ -69,61 +69,64 @@ IMAGES = ['''
         =========''', '''
 ''']
 
-WORDS =[
+WORDS = [
     'batman',
-     'superman',
-     'spiderman',
-     'joker'
+    'superman',
+    'joker',
+    'catwoman',
+    'spiderman',
+    'robin',
 ]
+
 
 def random_word():
     idx = random.randint(0, len(WORDS) - 1)
     return WORDS[idx]
 
+
 def display_board(hidden_word, tries):
-        print(IMAGES[tries])
-        print('')
-        print(hidden_word)
-        print('---*---*---*---')
+    print(IMAGES[tries])
+    print('')
+    print(hidden_word)
+    print('--- * --- * --- * --- * --- * --- ')
 
 
 def run():
     word = random_word()
-    hidden_word = ['-'] * len (word)
+    hidden_word = ['-'] * len(word)
     tries = 0
 
     while True:
         display_board(hidden_word, tries)
-        current_letter = str(input('escribe una letra:'))
+        current_letter = str(input('Escoge una letra: '))
 
-        letter_indexes = [] 
+        letter_indexes = []
         for idx in range(len(word)):
             if word[idx] == current_letter:
                 letter_indexes.append(idx)
 
-            if len(letter_indexes) == 0:
-                tries += 1
+        if len(letter_indexes) == 0:
+            tries += 1
 
-                if tries == 8:
-                    display_board(hidden_word, tries)
-                    print('')
-                    print('¡Perdiste! La palabra correcta es: {}'.format(word))
-                    break
+            if tries == 7:
+                display_board(hidden_word, tries)
+                print('')
+                print('¡Perdiste! La palabra correcta era {}'.format(word))
+                break
+        else:
+            for idx in letter_indexes:
+                hidden_word[idx] = current_letter
 
-                else:
-                    for idx in letter_indexes:
-                        hidden_word = current_letter
+            letter_indexes = []
 
-                        letter_indexes = []
-
-                        try:
-                            hidden_word.index('-')
-                        except ValueError:
-                            print('')
-                            print('¡Muy bien! Ganaste. La palabra es: {}'.format(word))
-                            break
+        try:
+            hidden_word.index('-')
+        except ValueError:
+            print('')
+            print('¡Felicidades! Ganaste. La palabra es: {}'.format(word))
+            break
 
 
 if __name__ == '__main__':
-     print('Bienvenidos a ahorcado')
-     run()
+    print('B I E N V E N I D O S  A  A H O R C A D O S')
+    run()
